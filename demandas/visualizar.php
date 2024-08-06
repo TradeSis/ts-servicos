59089bd85fe1cf1fb31fa161f645ecda49d4d7b9
Gabriel Bettencourt <gabriel.vieira@tradesis.com.br>
2024-08-06 16:03:18 -0300

    Update visualizar.php

--- demandas/visualizar.php
+++ demandas/visualizar.php
@@ -190,7 +190,7 @@ $statusEncerrar = array(
                             <button type="submit" form="my-form" class="btn btn-success">Atualizar</button>
                         </div>
                         <?php
-                        if ($usuario['idCliente'] == null) { ?>
+                        if ($usuario['idCliente'] == null && $demanda['idDemandaSuperior'] == null) { ?>
                         <div class="modal-footer">
                             <div class="col align-self-start pl-0">
                                 <button type="button" data-bs-toggle="modal" data-bs-target="#subdemandaModal" class="btn btn-info">Criar Subdemanda</button>
